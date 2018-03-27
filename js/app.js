90798a46b6e8cfc046f7d5d0a145bace21cb1ac9
ValeriaNiceria <valerianiceria@gmail.com>
2018-03-27 10:40:52 -0300

    Configurações da página

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAd9sDpBmcT5Q-MjEIQ_Ifhg_fKN3ucwj4",
+    authDomain: "estudo-firebase-6bcec.firebaseapp.com",
+    databaseURL: "https://estudo-firebase-6bcec.firebaseio.com",
+    projectId: "estudo-firebase-6bcec",
+    storageBucket: "estudo-firebase-6bcec.appspot.com",
+    messagingSenderId: "301171509059"
+};
+firebase.initializeApp(config);
